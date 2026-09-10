167371608724a05f1ce386815973afb550626f6d
Wout Stiens <71498452+StiensWout@users.noreply.github.com>
2026-09-10 05:49:36 +0200

    fix(web): retry markdown renderer when line wrapping changes

--- apps/web/src/components/ChatMarkdown.tsx
+++ apps/web/src/components/ChatMarkdown.tsx
@@ -2972,7 +2972,7 @@ const CHAT_MARKDOWN_COMPONENTS = {
       >
         {(wrapped) => (
           <RenderErrorBoundary
-            resetKeys={[codeBlock.code, language, diffThemeName, isStreaming]}
+            resetKeys={[codeBlock.code, language, diffThemeName, isStreaming, wrapped]}
             fallback={<pre {...props}>{children}</pre>}
           >
             <MarkdownCodeHighlight
