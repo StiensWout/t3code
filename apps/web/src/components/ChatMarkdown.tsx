d8a614ba0793c09d1d6413c059c506011632286b
Wout Stiens <71498452+StiensWout@users.noreply.github.com>
2026-09-13 20:26:59 +0200

    fix(web): remove obsolete markdown highlighter imports

--- apps/web/src/components/ChatMarkdown.tsx
+++ apps/web/src/components/ChatMarkdown.tsx
@@ -77,7 +77,6 @@ import type {
   Options as ReactMarkdownOptions,
 } from "react-markdown";
 import ReactMarkdown from "react-markdown";
-import { toHtml } from "hast-util-to-html";
 import { createIncrementalMarkdownPlugin } from "../markdown-incremental";
 import { defaultUrlTransform } from "react-markdown";
 import rehypeRaw from "rehype-raw";
@@ -131,8 +130,6 @@ import { openInEditorMenuLabel } from "../editorLabels";
 import { resolveDiffThemeName } from "../lib/diffRendering";
 import { MarkdownCodeHighlight } from "./MarkdownCodeHighlight";
 import { GitHubIcon } from "./Icons";
-import { createIncrementalHighlightedDocument } from "../lib/incrementalHighlighting";
-import { HighlightedCodeLines } from "./chat/HighlightedCodeLines";
 import { RenderErrorBoundary } from "./RenderErrorBoundary";
 import { useTheme } from "../hooks/useTheme";
 import { getClientSettings, useClientSettings } from "../hooks/useSettings";
